fd586a478c6932495f3d2d8168e0e6088aede273
Syed I.R <syed@lukonet.com>
2014-03-02 08:57:48 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+PHP XML-RPC URL Ping
+====================
+
+XML-RPC based Ping tool.
+
+More details coming soon!
---
+++ ping.php
@@ -0,0 +1,125 @@
+<?php
+
+$myBlogName = 'My Blog Title';
+$myBlogUrl = 'http://MyBlogDomain.com';
+$myBlogUpdateUrl = 'http://MyBlogDomain';
+$myBlogRSSFeedUrl = 'http://MyBlogDomain/feed';
+
+// Just and example so you need to put your own list here
+// I haven't used many of these for years
+// List of Servers to Ping
+$xmlRpcPingUrls[] = 'http://rpc.technorati.com/rpc/ping';
+$xmlRpcPingUrls[] = 'http://api.moreover.com/RPC2';
+$xmlRpcPingUrls[] = 'http://api.my.yahoo.com/RPC2';
+$xmlRpcPingUrls[] = 'http://blogupdate.org/ping/';
+$xmlRpcPingUrls[] = 'http://ping.bloggers.jp/rpc/';
+$xmlRpcPingUrls[] = 'http://ping.feedburner.com';
+$xmlRpcPingUrls[] = 'http://ping.syndic8.com/xmlrpc.php';
+$xmlRpcPingUrls[] = 'http://ping.weblogalot.com/rpc.php';
+$xmlRpcPingUrls[] = 'http://rpc.icerocket.com:10080/';
+$xmlRpcPingUrls[] = 'http://rpc.weblogs.com/RPC2';
+$xmlRpcPingUrls[] = 'http://services.newsgator.com/ngws/xmlrpcping.aspx';
+$xmlRpcPingUrls[] = 'http://www.blogpeople.net/servlet/weblogUpdates';
+$xmlRpcPingUrls[] = 'http://www.feedsky.com/api/RPC2';
+$xmlRpcPingUrls[] = 'http://ping.blogoon.net/';
+$xmlRpcPingUrls[] = 'http://ping.blogs.yandex.ru/RPC2';
+$xmlRpcPingUrls[] = 'http://ping.syndic8.com/xmlrpc.php';
+$xmlRpcPingUrls[] = 'http://www.blogpeople.net/ping';
+$xmlRpcPingUrls[] = 'http://www.blogpeople.net/servlet/weblogUpdates';
+$xmlRpcPingUrls[] = 'http://www.pubsub.com/ping';
+$xmlRpcPingUrls[] = 'http://www.syndic8.com/xmlrpc.php';
+$xmlRpcPingUrls[] = 'http://xping.pubsub.com/ping/';
+$xmlRpcPingUrls[] = 'http://api.moreover.com/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.com/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.ae/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.at/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.be/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.bg/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.ca/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.ch/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.cl/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.co.cr/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.co.hu/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.co.id/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.co.il/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.co.in/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.co.it/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.co.jp/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.co.ma/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.co.nz/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.co.th/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.co.uk/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.co.ve/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.co.za/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.com.ar/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.com.au/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.com.br/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.com.co/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.com.do/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.com.mx/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.com.my/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.com.pe/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.com.sa/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.com.sg/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.com.tr/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.com.tw/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.com.ua/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.com.uy/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.com.vn/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.com/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.de/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.es/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.fi/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.fr/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.gr/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.hr/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.ie/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.in/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.it/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.jp/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.lt/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.nl/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.pl/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.pt/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.ro/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.ru/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.se/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.sk/ping/RPC2';
+$xmlRpcPingUrls[] = 'http://blogsearch.google.tw/ping/RPC2';
+
+
+require_once( 'IXR_Lib.php' );
+
+function xmlRpcPing( $url ) {
+    global $myBlogName, $myBlogUrl, $myBlogUpdateUrl, $myBlogRSSFeedUrl;
+    $client = new IXR_Client( $url );
+    $client->timeout = 3;
+    $client->useragent .= ' -- XPOZiO/1.0.0';
+    $client->debug = false;
+
+    if( $client->query( 'weblogUpdates.extendedPing', $myBlogName, $myBlogUrl, $myBlogUpdateUrl, $myBlogRSSFeedUrl ) )
+    {
+        return $client->getResponse();
+    }
+
+    echo 'Failed extended XML-RPC ping for "' . $url . '": ' . $client->getErrorCode() . '->' . $client->getErrorMessage() . '<br />';
+
+    if( $client->query( 'weblogUpdates.ping', $myBlogName, $myBlogUrl ) )
+    {
+        return $client->getResponse();
+    }
+
+    echo 'Failed basic XML-RPC ping for "' . $url . '": ' . $client->getErrorCode() . '->' . $client->getErrorMessage() . '<br />';
+
+    return false;
+}
+
+foreach( $xmlRpcPingUrls as $url )
+{
+    echo '<h1>XML-RPC pinging ' . $url . '</h1>';
+    echo '<pre>';
+    print_r( xmlRpcPing( $url ) );
+    echo '</pre>';
+    ob_flush();
+    flush();
+}
